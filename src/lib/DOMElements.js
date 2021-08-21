f8d285f578aa193f7319f0502ddab617edf466e5
pikpokjeon <pikpokjeon@gmail.com>
2021-08-21 13:14:02 +0900

    feature 돔요소 생성자

---
+++ src/lib/DOMElements.js
@@ -0,0 +1,22 @@
+import { fragment, createEl, loop, If, pipe } from "./lib/lib"
+
+export const Container = createEl('div', { class: 'container' })
+export const ImgBox = createEl('div',{class:'img-box'})
+export const InfoBox = createEl('div',{class:'info-box'})
+export const Badge =  createEl('div', { class: 'badge'})
+
+export const div = createEl('div')
+export const ul = createEl('ul')
+export const li = createEl('li')
+export const span = createEl('span')
+export const p = createEl('p')
+export const Img = createEl('img')
+export const section = createEl('section')
+export const article = createEl('article')
+
+export const flexBox = {
+    center: createEl('div', { class: 'flex-all-center' }),
+    spaceBetween: createEl('div', { class: 'flex-spaceBetween' }),
+    colum:createEl('div',{class:'flex-column'}),
+    row:createEl('div',{class:'flex-row'}),
+}
