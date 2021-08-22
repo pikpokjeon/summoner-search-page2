58e4e434202c4e049f7a395f5e2021cdd089288a
pikpokjeon <pikpokjeon@gmail.com>
2021-08-22 17:57:39 +0900

    feature 프로필 컴포넌트

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,7 +1,7 @@
 import {fragment,createEl, _} from '../lib/lib'
-import { Container, ImgBox, div ,Badge, span, li, Img} from '../lib/DOMElements'
-import { img } from '../textResource'
-
+import { Container, ImgBox, div ,Badge, span, li, Img,flexBox} from '../lib/DOMElements'
+import profileImg from '../../assets/img/profileImg.jpg'
+import frameImg from '../../assets/img/frame.png'
 
 const BadgeContainer = createEl('ul',{class:'tier-badges'})
 
@@ -9,19 +9,28 @@ const Profile = (store) => {
 
     const { keyword, summoner } = store.getState()
 
-        const Badges = summoner.previousTiers.reduce((acc,cur)=> [...acc,
-            li([ Badge ([ span(`${cur.shortString} ${cur.tier}`)])])],[]) 
-
+    const Badges = summoner?.previousTiers.reduce((acc,cur)=> [...acc,
+        li([ 
+            Badge ([
+                    span({class:'t-11 p-dark-light accent ls-4'},
+                    `${cur.shortString} ${cur.tier}`)])])],[]) 
     return (
         fragment([
             Container( { class: 'profile-container' }, [
-            BadgeContainer([...Badges]),
+            BadgeContainer([...Badges??[]]),
             div({class:'player-info '},[
-                ImgBox([
-                    Img({src: img.profileImg}), 
-                ]),div(`${keyword}`)
+                ImgBox({class:'background-profile'},[
+                    Img({src:`${frameImg}`,class:'frame'}),
+                    Img({src:`${profileImg}`,class:'profile'})
+                ]),
+                flexBox.center([div({class:'t-20 accent p-dark ls-7'},`${keyword}`),
+                div({class:'profile-info'},[
+                    span({class:'t-13 p-light'},`래더랭킹${keyword}`)]),
+
             ]),
+        ]),
         ])]))
+    
 }
 
 
